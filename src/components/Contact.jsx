fb76d1706b989898f5059ecc6df7d708a725d061
Anju Karanji <anjukaranji@gmail.com>
2025-09-18 14:56:48 -0400

    Fix netlify contact form attributes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -59,8 +59,9 @@ export default function Contact() {
           </div>
         </div>
         <form
-          netlify
           name="contact"
+          data-netlify="true"
+          method="POST"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
@@ -79,6 +80,7 @@ export default function Contact() {
               type="text"
               id="name"
               name="name"
+              autoComplete="name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               onChange={(e) => setName(e.target.value)}
             />
@@ -91,6 +93,7 @@ export default function Contact() {
               type="email"
               id="email"
               name="email"
+              autoComplete="email"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               onChange={(e) => setEmail(e.target.value)}
             />
@@ -105,6 +108,7 @@ export default function Contact() {
             <textarea
               id="message"
               name="message"
+              autoComplete="off"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
               onChange={(e) => setMessage(e.target.value)}
             />
